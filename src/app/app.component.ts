7f24363e80583bc2b386326ecd9295104d33c228
Pranital <laddha.pranita1988@gmail.com>
2021-02-09 03:36:16 +0530

    update in resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { $ } from 'protractor';
+import * as jquery from 'jquery'  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pranitaresume';
+  title = 'Pranita Laddha - Front End Developer';
+  isClicked = 1;
+  links = [
+    'home',
+    'about',
+    'resume',
+    'portfolio',
+    'services',
+    'contact'
+  ];
+
+  constructor(){
+    console.log()
+  }
+
+  increaseCount() {
+    this.isClicked += 1;
+    console.log('click: ' , this.isClicked)
+  }
+
+  @HostListener('click') onClick(e: any) {
+    console.log(e)
+    // jquery('.navbar-collapse.collapse').removeClass('show');
+    // jquery('.menu-toggle.isClicked').removeClass('isClicked')
+  } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
